01d3316a928fa3c5b11d7cf7cc4dd0cf02a0d291
Seder3214 <50592797+Seder3214@users.noreply.github.com>
2024-01-12 23:49:01 +0300

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -77,7 +77,7 @@ if(hasUpgrade("ap",11))b=b.mul(upgradeEffect("ap",11));
 if(player.t.activeChallenge==11||player.t.activeChallenge==21||player.t.activeChallenge==31)b=b.pow(tmp.t.dilationEffect);
 if(player.ap.activeChallenge==22 ||player.ap.activeChallenge==41||player.ap.activeChallenge==42 )b=b.add(1).log10().pow(player.m.best.gte(122)?player.m.points:100);
 if (player.mp.activeChallenge==21) b = new Decimal(1).div(player.mp.points.log2().pow(0.5))
-if (player.pm.essence.gte(1)) b = b.mul(tmp.pm.essenceBoost)
+if (player.pm.essence.gte(1) && player.mp.activeChallenge==21) b = b.mul(tmp.pm.essenceBoost)
 return b.div(player.m.best.gte(180)?2:1)
 }
 
