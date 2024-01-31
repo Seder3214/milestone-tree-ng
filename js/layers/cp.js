48df92fa929152f246ff7a9cc9058d7501c92b26
Seder3214 <50592797+Seder3214@users.noreply.github.com>
2024-01-31 10:52:02 +0300

    Update cp.js

--- js/layers/cp.js
+++ js/layers/cp.js
@@ -178,8 +178,8 @@ canBuyMax() {return true},
                         'border-color':'linear-gradient(to left top, lime, #52f552)',
                         'background':'#0f0f0f',
                         'right': '0%',
-                        'left':'280%',
-                        'bottom': '-10%',
+                        'left':'260%',
+                        'bottom': '-15%',
                         'width':'300px',
                     };
                     case "left":
@@ -189,7 +189,7 @@ canBuyMax() {return true},
                             'background':'#0f0f0f',
                             'right': '0%',
                             'left':'-170%',
-                            'bottom': '-10%',
+                            'bottom': '-15%',
                             'width':'300px',
                         };
                     case "bottom":
