6092d796b5604769e5abd29a5834c655b75df9f6
Seder3214 <50592797+Seder3214@users.noreply.github.com>
2024-03-24 19:49:31 +0300

    Update pep.js

--- js/layers/pep.js
+++ js/layers/pep.js
@@ -36,7 +36,7 @@ addLayer("pep", {
         return eff;
     },
 	prThreeEffect() {
-        let eff = player.pep.points.log10().pow(0.35).mul(player.pep.points.div(100))
+        let eff = player.pep.points.add(1).log10().pow(0.35).mul(player.pep.points.div(100))
         return eff;
     },
     row: 1, // Row the layer is in on the tree (0 is the first row)
