2feb95ee6fb992510386705d506d799b49311c0f
Seder3214 <50592797+Seder3214@users.noreply.github.com>
2024-03-25 21:06:17 +0300

    Update cp.js

--- js/layers/cp.js
+++ js/layers/cp.js
@@ -358,9 +358,9 @@ canBuyMax() {return true},
            if (data.type=='div') eff = new Decimal(data.level).add(1).mul(3).pow(1+data.level/10)
            if (data.type=='pm') eff = new Decimal(data.level).add(1).mul(1.5).pow(1+data.level/10)
            if (data.level>=15 && data.type=='pm') eff = eff.mul(25)
-           if (data.level>=20 && data.type=='pm') eff = eff.mul(new Decimal(data.level).div(2).pow(2.5))
+           if (data.level>=20 && data.type=='pm') eff = eff.mul(new Decimal(data.level).div(2).pow(2))
 
-           if (data.level>=10 && data.type=='div') eff = eff.mul(new Decimal(data.level).div(data.level>=25?10:5))
+           if (data.level>=10 && data.type=='div') eff = eff.mul(new Decimal(data.level).div(data.level>=25?20:10))
             return eff.mul(new Decimal(data.cautPower).add(1))},
         onClick(data, id) { 
             if (data.level>=1) {player[this.layer].grid[id].active=!player[this.layer].grid[id].active
