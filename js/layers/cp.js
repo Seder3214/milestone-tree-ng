3af9e1a10196d133278cc55a721e5339f62d29af
Seder3214 <50592797+Seder3214@users.noreply.github.com>
2024-04-01 22:56:49 +0300

    Update cp.js

--- js/layers/cp.js
+++ js/layers/cp.js
@@ -91,10 +91,9 @@ canBuyMax() {return true},
         for (i=0;i<tmp.cp.resetGain;i++) {
         let grid = player.cp.grid
         let slots = Object.keys(grid).filter(x => grid[x].level<1 && (Math.floor(x/100)<=tmp.cp.grid.rows && (x%10)<=tmp.cp.grid.cols))
-        if (slots[slots.length-1]=="") slotsFixed=slots.pop()
         if (slots.length) {
             let slot = slots[Math.floor(Math.random() * slots.length)]
-if (player.cp.grid[slot].level>=1) slot = slots[Math.floor(Math.random() * slots.length)]
+slotsFixed=slots.pop(slot)
             let rangeMul = Math.floor(player.cp.totalCorrupt/4)
             let addLevel = Math.floor(player.cp.totalCorrupt/5)*1.5
             let ranType = Math.floor(Math.random()*1.5)
